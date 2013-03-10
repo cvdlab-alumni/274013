d9f62f5db1c5a15224283ae130882c5328a7f48a
sbkgladiator <g.gladiator@tiscali.it>
2013-03-10 17:17:23 +0100

    message

--- learning-javascript/2013-3-9/exercise03.js
+++ learning-javascript/2013-3-9/exercise03.js
@@ -9,7 +9,6 @@ var t="";
 
 if((s.charAt(0)) ===("\"" || " ")) 
 return s.charAt(0).toUpperCase()+cap(s.slice(1))
-else return s.charAt(0)
-+cap(s.slice(1)); }
+else return s.charAt(0)+cap(s.slice(1)); }
 
 console.log(cap("\"hello world"));
