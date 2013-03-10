fc1a1943cf4b09aa3eea39a3b8c473fc9fa131b5
sbkgladiator <g.gladiator@tiscali.it>
2013-03-10 17:09:39 +0100

    message

--- learning-javascript/2013-3-9/exercise03.js
+++ learning-javascript/2013-3-9/exercise03.js
@@ -7,10 +7,9 @@ console.log(upp("ciao"));
 function cap(s){
 var t="";
 
-if((s.charAt(0)) ==="\"") 
-return (s.charAt(1)).toUpperCase() + cap(2)
-else if(s.charAt(0)===" ") 
-return " "+s.charAt(1).toUpperCase()+cap(2)
-else return s.charAt(1)+cap(2); }
+if((s.charAt(0)) ===("\"" || " ")) 
+return s.charAt(0).toUpperCase()+cap(s.slice(1))
+else return s.charAt(0)
++cap(s.slice(1)); }
 
 console.log(cap("\"hello world"));
