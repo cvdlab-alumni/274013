af5083d72c7b8e542095b4e594fe24e17912aeb6
sbkgladiator <g.gladiator@tiscali.it>
2013-03-23 17:28:16 +0100

    esercizi

--- learning-javascript/2013-3-21/exercise04.js
+++ learning-javascript/2013-3-21/exercise04.js
@@ -1,14 +1,3 @@
-var data = [
-  {id:'01', name:'duffy'},
-  {id:'02', name:'michey'},
-  {id:'03', name:'donald'},
-  {id:'04', name:'goofy'},
-  {id:'05', name:'minnie'},
-  {id:'06', name:'scrooge'}
-];
-var key = 'name';
-var values = ['goofy', 'scrooge'];
-
-select(data, key, values){
-
-}
+function area(t){
+return Math.sqrt(perimetro(t)
+*(perimetro(t)-t.a)*(perimetro(t)-t.b)*(perimetro(t)-t.c));}
