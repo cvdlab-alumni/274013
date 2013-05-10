d419693c9f23ccffde119507690b956c9e36e0d8
giorgio <giorgio@ubuntu.ubuntu-domain>
2013-05-11 00:08:41 +0200

    hw2

--- 2013-05-10/python/exercise3.py
+++ 2013-05-10/python/exercise3.py
@@ -7,6 +7,6 @@ def point2D(x,y):
 	x=x
 	y=y
 
+gomma=COLOR(BLACK)(DIFFERENCE([ CYLINDER([1.0,1.0])(80), CYLINDER([0.8,1.0])(80) ]))
 
-
-VIEW(DIFFERENCE([ CYLINDER([1.0,2.0])(80), CYLINDER([0.8,2.0])(80) ]))
+VIEW(gomma)
