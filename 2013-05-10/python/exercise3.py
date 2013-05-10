5cc06227500c2eeca87f88039e9fe21375da9dc0
giorgio <giorgio@ubuntu.ubuntu-domain>
2013-05-11 00:21:45 +0200

    hw2

--- 2013-05-10/python/exercise3.py
+++ 2013-05-10/python/exercise3.py
@@ -4,4 +4,5 @@ from pyplasm import *
 
 gomma=COLOR(BLACK)(DIFFERENCE([ CYLINDER([1.0,1.0])(80), CYLINDER([0.8,1.0])(80) ]))
 cerchio=COLOR(YELLOW)(STRUCT([ CYLINDER([0.3,0.2])(80),DIFFERENCE([ CYLINDER([0.8,1.0])(80), CYLINDER([0.7,1.0])(80) ]) ]))
-VIEW(STRUCT([gomma,cerchio]))
+weel=STRUCT([gomma,cerchio])
+VIEW(weel)
