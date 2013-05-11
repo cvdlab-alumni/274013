5bb7376026bf026d961c9c7b84acd389fb3f9e6b
sbkgladiator <g.gladiator@tiscali.it>
2013-05-11 12:20:19 +0200

    hw2

--- 2013-05-10/python/exercise4.py
+++ 2013-05-10/python/exercise4.py
@@ -1,7 +1,7 @@
 from pyplasm import *
 
-toro=TORUS([10,12])([50,50])
-cil=CYLINDER([2.0,1.0])(50) 
+toro=COLOR(BLACK)(TORUS([10,12])([50,50]))
+cil=DIFFERENCE([ CYLINDER([2.0,0.5])(50) ,CYLINDER([1.0,0.2])(50) ])
 volante=STRUCT([toro,cil])
 
 
