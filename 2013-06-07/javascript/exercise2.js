09dcccca0c9d18a3f59fd23fe8a4eb08d4ed52f4
sbkgladiator <g.gladiator@tiscali.it>
2013-06-08 15:40:00 +0200

    hw3

--- 2013-06-07/javascript/exercise2.js
+++ 2013-06-07/javascript/exercise2.js
@@ -11,10 +11,10 @@ var piano = new Array();
 
 function terr(alt){
 var z=1.5-Math.random();
-piano[alt[0],alt[1]]=z
+piano[alt[0],alt[1]]=z;
 	return [alt[0],  alt[1], z];
 }
-var model =MAP(terr)(dom)
+var model =MAP(terr)(dom);
 DRAW(COLOR([188/255, 152/255, 126/255])(model));
 
 
--- 2013-06-07/javascript/exercise5.js
+++ 2013-06-07/javascript/exercise5.js
@@ -13,9 +13,9 @@ var piano = new Array();
 var v=Array()
 function terr(alt){  //codice provenente da modifica della mia funzione dell'arco di circonferenza
 var z=1.5-Math.random();
-piano[alt[0],alt[1]]=z
+piano[alt[0],alt[1]]=z;
 var r=[alt[0],  alt[1], z];
-v.push(r)
+v.push(r);
 	return [alt[0],  alt[1], z];
 }
 var model =MAP(terr)(dom)
